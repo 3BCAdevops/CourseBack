13e6d6d3f9a490bc08cebf2c6c4ceddc5766ad1b
Hemavathypandiarajan <23suca35@tcarts.in>
2026-01-31 22:46:25 +0530

    url

--- src/main/java/course_enrollment/controller/CourseController.java
+++ src/main/java/course_enrollment/controller/CourseController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/courses")
 @CrossOrigin(origins = "*")
 public class CourseController {
 
